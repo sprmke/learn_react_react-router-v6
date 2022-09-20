12d3445c925fdf4fdbc4b934d6cbc70322e6bccb
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-20 11:02:56 +0800

    lazy load pages and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React, { Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+const Welcome = React.lazy(() => import('./pages/Welcome'));
+const Products = React.lazy(() => import('./pages/Products'));
+const ProductDetail = React.lazy(() => import('./pages/ProductDetail'));
+const MainHeader = React.lazy(() => import('./components/MainHeader'));
 
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Routes>
-          <Route path='/' element={<Navigate replace to='/welcome' />} />
-          <Route path='/welcome/*' element={<Welcome />}>
-            <Route path='new-user' element={<p>Welcome, new user!</p>} />
-          </Route>
-          <Route path='/products' element={<Products />} />
-          <Route path='/products/:productId' element={<ProductDetail />} />
-        </Routes>
-      </main>
+      <Suspense
+        fallback={
+          <div className='centered'>
+            <p>Loading...</p>
+          </div>
+        }
+      >
+        <MainHeader />
+        <main>
+          <Routes>
+            <Route path='/' element={<Navigate replace to='/welcome' />} />
+            <Route path='/welcome/*' element={<Welcome />}>
+              <Route path='new-user' element={<p>Welcome, new user!</p>} />
+            </Route>
+            <Route path='/products' element={<Products />} />
+            <Route path='/products/:productId' element={<ProductDetail />} />
+          </Routes>
+        </main>
+      </Suspense>
     </div>
   );
 }
